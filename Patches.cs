4f0efbfc0937fc9173f2943344e07807ff1edd9f
Joe Christensen <chri4665@d.umn.edu>
2025-03-09 11:11:32 -0700

    V1

---
+++ Patches.cs
@@ -0,0 +1,41 @@
+﻿using HarmonyLib;
+
+namespace FartMine.Patches
+{
+    //[HarmonyPatch(typeof(ParticleScriptExplosion), "Spawn")]
+    //public class ParticleScriptExplosionPatch
+    //{
+    //    public static void Prefix(ParticleScriptExplosion __instance)
+    //    {
+
+    //    }
+    //}
+
+    //[HarmonyPatch(typeof(ItemMineExplosive), "OnTriggered")]
+    //public class ItemMineExplosivePatch
+    //{
+    //    [HarmonyPrefix]
+    //    public static bool Prefix(ItemMineExplosive __instance, ParticleScriptExplosion ___particleScriptExplosion)
+    //    {
+    //        Plugin.Log.LogInfo("Mine triggered! :)");
+    //        Plugin.Log.LogInfo($"Position: {__instance.transform.position}");
+
+    //        ___particleScriptExplosion.Spawn(__instance.transform.position, 1.2f, 75, 200, 4f, false, true, 1f);
+    //        return false;
+    //    }
+    //}
+
+    [HarmonyPatch(typeof(ItemMineExplosive), "Start")]
+    public class ItemMineExplosivePatch
+    {
+        [HarmonyPostfix]
+        public static void Postfix(ItemMineExplosive __instance, ParticleScriptExplosion ___particleScriptExplosion)
+        {
+            Plugin.Log.LogInfo("Mine triggered! :)");
+            Plugin.Log.LogInfo($"Position: {__instance.transform.position}");
+
+            ___particleScriptExplosion.explosionPreset.explosionSoundBig.Sounds = Plugin.SoundFX.ToArray();
+            ___particleScriptExplosion.explosionPreset.explosionSoundBigGlobal.Sounds = Plugin.SoundFX.ToArray();
+        }
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,54 @@
+﻿using BepInEx;
+using HarmonyLib;
+using BepInEx.Logging;
+using UnityEngine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+
+namespace FartMine
+{
+    [BepInPlugin(modGUID, modName, modVersion)]
+    public class Plugin : BaseUnityPlugin
+    {
+        public const string modGUID = "Eteli.FartMine";
+        public const string modName = "Fart Mine";
+        public const string modVersion = "0.0.1";
+
+        private static Harmony _harmony;
+
+        internal static new ManualLogSource Log;
+
+        internal static List<AudioClip> SoundFX;
+        internal static AssetBundle Bundle;
+
+        public void Awake()
+        {
+            Log = base.Logger;
+            Log.LogInfo($"{modGUID} has awoken!");
+
+            _harmony = new Harmony(modGUID);
+            _harmony.PatchAll();
+
+
+            SoundFX = new List<AudioClip>();
+
+            string FolderLocation = this.Info.Location;
+            FolderLocation = FolderLocation.TrimEnd("FartMine.dll".ToCharArray());
+            Bundle = AssetBundle.LoadFromFile(FolderLocation + "fartmine");
+            if (Bundle != null)
+            {
+                SoundFX = Bundle.LoadAllAssets<AudioClip>().ToList();
+
+                Log.LogInfo("Successfully loaded thge asset bundle");
+
+                Log.LogInfo($"Loaded {SoundFX.Count} sound effects");
+            }
+            else
+            {
+                Log.LogError("Failed to load asset bundle!");
+            }
+        }
+    }
+}
